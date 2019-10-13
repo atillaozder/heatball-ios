5526cba537744f508980fcac487aeaae776df819
Atilla Özder <atillanevzat@gmail.com>
2019-10-13 18:20:42 +0300

    v1.0.1

--- src/GameViewController.swift
+++ src/GameViewController.swift
@@ -83,7 +83,7 @@ class GameViewController: UIViewController {
     }
     
     override var shouldAutorotate: Bool {
-        return true
+        return false
     }
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
