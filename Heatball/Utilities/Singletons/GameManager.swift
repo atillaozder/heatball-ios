b3a080f98b6fea6fae3e892c97595d014c83debe
Atilla Özder <atillanevzat@gmail.com>
2020-09-25 15:27:10 +0300

    v1.1.3 Build 12

--- Heatball/Utilities/Singletons/GameManager.swift
+++ Heatball/Utilities/Singletons/GameManager.swift
@@ -65,7 +65,7 @@ final class GameManager: NSObject {
         if defaults.shouldRequestGCAuthentication {
             let localPlayer = GKLocalPlayer.local
             localPlayer.authenticateHandler = { [weak self] (viewController, error) in
-                guard let `self` = self else { return }
+                guard let self = self else { return }
                 if viewController != nil {
                     presentingViewController.present(viewController!, animated: true, completion: nil)
                     defaults.setGCRequestAuthentication()
